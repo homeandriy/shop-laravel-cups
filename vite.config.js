99937cea017c82c1a3af4dbc75bafde6abac3f95
Andriy Beznosko <a.beznosko@temabit.com>
2023-09-07 22:41:42 +0300

    Prepare shop -Add views - Fix vite JS

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/admin/app.css',
                 'resources/js/app.js',
+                'resources/js/admin/app.js',
             ],
             refresh: true,
         }),
